773b25470c3cefd99dc864afaf33229f5100e862
kazukitidesu4649 <kazukitidesu@gmail.com>
2026-05-21 14:34:30 +0900

    first commit

---
+++ src/text/section_04/Data_8.java
@@ -0,0 +1,8 @@
+package text.section_04;
+
+public class Data_8 {
+	public static void main(String[] args) {
+		System.out.println("岩橋" + "和樹" + 123);
+	}
+
+}
---
+++ src/text/section_04/Data_9.java
@@ -0,0 +1,9 @@
+package text.section_04;
+
+public class Data_9 {
+	public static void main(String[] args) {
+		System.out.println("岩橋和樹" + (123 + 654));
+		System.out.println(123 + 654 + "岩橋和樹");
+	}
+
+}
