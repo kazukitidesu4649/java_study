9d2b49fccbe30ef66317e2ddbc6e8187e6808398
kazukitidesu4649 <kazukitidesu@gmail.com>
2026-06-09 14:41:39 +0900

    for文10を超えるまで足し算

---
+++ src/text/section_10/java/ForSyntax_01.java
@@ -0,0 +1,13 @@
+package text.section_10.java;
+
+public class ForSyntax_01 {
+	public static void main(String[] args) {
+		
+		// 変数i=1~10の範囲で繰り返す
+		for( int i = 1; i<11; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
